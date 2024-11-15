afbbc4f34a41cfcc158a8d2ee64e6ec1284675d1
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2024-11-14 21:03:39 -0300

    initial commit

---
+++ src/main/java/com/app/acessibilidade/api/domain/model/Usuario.java
@@ -0,0 +1,36 @@
+package com.app.acessibilidade.api.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded=true)
+@AllArgsConstructor
+
+
+public class Usuario {
+    
+    @Id
+    // indetity -> forma nativa do sgbd(no caso auto increment.Para casos em que o banco é criado manualmente. AUTO é para quando for criando junto com a API)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name="id")
+    private Long id;
+    
+    private  String nome;
+    
+    private  String email;
+
+    private  String senha;
+
+}
