f0b31932934ed57df09df664cf777954713de6c8
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2024-11-16 20:20:33 -0300

    BugFix: cadrastro de avaliacao -acesso a atributo null-erro 500

--- src/main/java/com/app/acessibilidade/api/domain/service/AvaliacaoService.java
+++ src/main/java/com/app/acessibilidade/api/domain/service/AvaliacaoService.java
@@ -26,7 +26,8 @@ public class AvaliacaoService {
             return null;
         }
         Avaliacao avaliacao=buscaPelaAvaliacao.get();
-        return new OUTPUT_Avaliacao_DTO(avaliacao.getId(), avaliacao.getComentario(), avaliacao.getEstrelas(), avaliacao.getDt_avaliacao(), avaliacao.getUsuario().getId(), avaliacao.getLocal().getId());
+        var usuario=(avaliacao.getUsuario()!=null)? avaliacao.getUsuario().getId() : null  ;
+        return new OUTPUT_Avaliacao_DTO(avaliacao.getId(), avaliacao.getComentario(), avaliacao.getEstrelas(), avaliacao.getDt_avaliacao(), usuario, avaliacao.getLocal().getId());
 
     }
     public List<OUTPUT_Avaliacao_DTO> ListarAvaliacao(){
@@ -37,13 +38,17 @@ public class AvaliacaoService {
        
        Avaliacao avaliacao_sem_id=new Avaliacao(null,avaliacaoDTO.comentario(), avaliacaoDTO.estrelas(), avaliacaoDTO.dt_avaliacao(), avaliacaoDTO.usuario(), avaliacaoDTO.local(), null);
        Avaliacao avaliacao_com_id=avaliacaoRepository.save(avaliacao_sem_id);
-        return new OUTPUT_Avaliacao_DTO(avaliacao_com_id.getId(), avaliacao_com_id.getComentario(), avaliacao_com_id.getEstrelas(), avaliacao_com_id.getDt_avaliacao(), avaliacao_com_id.getUsuario().getId(), avaliacao_com_id.getLocal().getId());
+
+       var usuario=(avaliacao_com_id.getUsuario()!=null)? avaliacao_com_id.getUsuario().getId() : null  ;
+
+        return new OUTPUT_Avaliacao_DTO(avaliacao_com_id.getId(), avaliacao_com_id.getComentario(), avaliacao_com_id.getEstrelas(), avaliacao_com_id.getDt_avaliacao(), usuario, avaliacao_com_id.getLocal().getId());
     }
     @Transactional
     public OUTPUT_Avaliacao_DTO editarAvaliacao(INPUT_Avaliacao_DTO avaliacaoDTO){
         Avaliacao avaliacao_sem_id=new Avaliacao(null,avaliacaoDTO.comentario(), avaliacaoDTO.estrelas(), avaliacaoDTO.dt_avaliacao(), avaliacaoDTO.usuario(), avaliacaoDTO.local(), null);
         Avaliacao avaliacao_com_id=avaliacaoRepository.save(avaliacao_sem_id);
-         return new OUTPUT_Avaliacao_DTO(avaliacao_com_id.getId(), avaliacao_com_id.getComentario(), avaliacao_com_id.getEstrelas(), avaliacao_com_id.getDt_avaliacao(), avaliacao_com_id.getUsuario().getId(), avaliacao_com_id.getLocal().getId());
+        var usuario=(avaliacao_com_id.getUsuario()!=null)? avaliacao_com_id.getUsuario().getId() : null  ;
+         return new OUTPUT_Avaliacao_DTO(avaliacao_com_id.getId(), avaliacao_com_id.getComentario(), avaliacao_com_id.getEstrelas(), avaliacao_com_id.getDt_avaliacao(), usuario, avaliacao_com_id.getLocal().getId());
      }
     @Transactional
     public void excluirAvaliacao(Long avaliacaoId){
